391d27b415e613b0302ef85e11a15d125d8c3e42
4Vald <alekezhan67@gmail.com>
2025-10-21 15:29:42 +0500

    второе задание: 21.10

--- ModelsApp/models.py
+++ ModelsApp/models.py
@@ -66,9 +66,20 @@ class Article(models.Model):
         return self.title
     
 
-    
+
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
+    
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    author_name = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author_name} on {self.article.title}"
