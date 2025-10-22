ce55c027e914e5f4d8d48e021320d75339de599b
4Vald <alekezhan67@gmail.com>
2025-10-22 15:14:47 +0500

    второе задание 22.10

--- ModelsApp/models.py
+++ ModelsApp/models.py
@@ -39,41 +39,41 @@ from django.db import models
 # | `help_text="..."`      | Подсказка в админке                  |
 
 
-
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    bio = models.CharField(max_length=100, default="Биография пока не заполнена")  # default применён здесь
-    created_at = models.DateTimeField(auto_now_add=True)  # убран default
+    bio = models.CharField(max_length=100, default="Биография пока не заполнена")
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
     
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-    
-
-    
-class Article(models.Model):
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='articles')
-    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True, related_name='articles')
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-    
-
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
-    
 
+class Tag(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='articles')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='articles')
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag, blank=True, related_name='articles') 
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
